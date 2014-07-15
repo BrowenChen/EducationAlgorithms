ced745a0be6e0e1e48b79b8cd6e69fbb6cad1acf
Browenchen <owenchen@berkeley.edu>
2014-07-15 22:59:03 +0100

    algorithms webapp

---
+++ eduWeb/app.js
@@ -0,0 +1,79 @@
+
+/**
+ * Module dependencies
+ */
+
+var express = require('express'),
+  bodyParser = require('body-parser'),
+  methodOverride = require('method-override'),
+  errorHandler = require('error-handler'),
+  morgan = require('morgan'),
+  routes = require('./routes'),
+  api = require('./routes/api'),
+  http = require('http'),
+  path = require('path');
+
+var app = module.exports = express();
+
+
+/**
+ * Configuration
+ */
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(morgan('dev'));
+app.use(bodyParser());
+app.use(methodOverride());
+app.use(express.static(path.join(__dirname, 'public')));
+
+var env = process.env.NODE_ENV || 'development';
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+/**
+ * Routes
+ */
+
+// serve index and view partials
+app.get('/', routes.index);
+app.get('/partials/:name', routes.partials);
+
+// JSON API
+app.get('/api/name', api.name);
+
+// redirect all others to the index (HTML5 history)
+app.get('*', routes.index);
+
+
+/**
+ * Start Server
+ */
+
+http.createServer(app).listen(app.get('port'), function () {
+  console.log('Express server listening on port ' + app.get('port'));
+});
---
+++ eduWeb/public/js/app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+// Declare app level module which depends on filters, and services
+
+angular.module('myApp', [
+  'myApp.controllers',
+  'myApp.filters',
+  'myApp.services',
+  'myApp.directives'
+]).
+config(function ($routeProvider, $locationProvider) {
+  $routeProvider.
+    when('/view1', {
+      templateUrl: 'partials/partial1',
+      controller: 'MyCtrl1'
+    }).
+    when('/view2', {
+      templateUrl: 'partials/partial2',
+      controller: 'MyCtrl2'
+    }).
+    when('/land', {
+      templateUrl: 'partials/land',
+      controller: 'LandCtrl'
+    }).    
+    otherwise({
+      redirectTo: '/view1'
+    });
+
+  $locationProvider.html5Mode(true);
+});
