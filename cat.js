0963900c86020f0875925ff71eb8ed7a64a175d6
Browenchen <owenchen@berkeley.edu>
2014-07-17 13:57:15 +0100

    implemetned and tested 10 steps of algorithm

--- cat.js
+++ cat.js
@@ -53,7 +53,8 @@ A Methodology Whose Time Has Come.
 */
 
 var fs = require('fs');
-var readline = require('fs');
+var readline = require('readline');
+var readlineSync = require('readline-sync');
 
 
 //JSON card bank
@@ -69,32 +70,55 @@ var D = 0,
 	H = 0,
 	R = 0;
 
+var student = {
+	name: "Owen",
+	ability: 25,
+}; //Student Object
+var item = "";
 
 // var itemBank = []; // Item Bank to choose from
+// var candidateBank = []; //student Test bank
 
-// Dummy Data
+// Dummy Data =====================================================
 var itemBank = [ 
 	{
 		question: "test1",
+		answer: "1",
 		difficulty: 9
 	},
 	{
 		question: "test3",
+		answer: "1",		
 		difficulty: 92
 	},
 	{
-	  	question: "test4",
+	  	question: "test4: This is a question",
+	  	answer: "1",		
 	  	difficulty: 19
 	},		
 	{
 		question: "test2",
+		answer: "1",	
 		difficulty: 14
 	}
 ]; // Item Bank to choose from
 
+var candidateBank = [
+	{
+		name: "Owen",
+		ability: 25
+	},
 
-var student = {}; //Student Object
-var item = "";
+	{
+	 	name: "Chris",
+	 	ability: 15
+	}	
+
+]; //student Test bank
+
+// Dummy Data ====================================================================
+
+
 
 
 /**
@@ -121,8 +145,55 @@ Set user's D to the actual D value of that item.
 **/
 
 var begin = function(){
-	//Prompt for next candidate 
+	// St 1) Prompt for next candidate 
+	var nextCandidate = candidateBank[0];
+
+	D = 0;
+	L = 0;
+	H = 0;
+	R = 0;
+
+	// St 2) Find closest difficulty
+	var closestItem = findItemInBank(nextCandidate.ability);
+	// St 3) Set D at the actual calibration of that item
+	D = closestItem.difficulty;
+	console.log(D);
+
+	// St 4-5) Administer the Item. Continue steps in here
+	var response = administerItem(closestItem);
+	// console.log(response);
+
+	// St 6) Score this response. 1 = right, 0 - wrong.
+	
+	var score = scoreResponse(response, closestItem);
+	console.log(score);
+
+	// St 7) Total items taken
+	L += 1;
+
+	// St 8) Add total difficulties used
+	H = H + D;
+
+
+	// St 9) If response incorrect, D = D - 2/L
+	if (score == 0){
+		console.log(D + "\n Before");
+		D = D - 2 / L;
+		console.log(D);
+	}
+
+	// St 10) If response is correct, D = D + 2/L
+	if (score == 1){
+		console.log("Correct ");
+		D = D + 2/L;
+		R += 1; // Update right count
+	}
+	
+
+
+
 };
+
 
 
 /**
@@ -145,6 +216,60 @@ var findItemInBank = function(D){
 	return minItem
 }
 
+/**
+@administerItem
+@param item -> dictionary of question and difficulty
+
+Gives the user the question, recieves a response.
+**/
+
+var administerItem = function(item){
+	var question = item.question;
+	return getUserPrompt(question, item); //Asynchronous call
+
+}
+
+/**
+@getUserPrompt
+@param question
+
+Prompts the user with the question. Synchronous version with readlineSync
+**/
+
+var getUserPrompt = function(question, item){
+
+	var answer = readlineSync.question(question + '\n' );
+
+	console.log("Your answer is: \n");
+
+	return answer;
+
+	 
+}
+
+
+
+/**
+@scoreResponse
+@params reply - Reply from command Line
+			- Item - Current dictionary object with the item
+Scores the response 
+**/
+
+var scoreResponse = function(response, item){
+	console.log("Scoring response");
+	if (response != item.answer){
+		console.log("WRONG");
+		return 0;
+	}
+
+	else {
+		console.log("Correct");
+		return 1;
+	}
+	
+};
+
 
 /**
 @IRTAlgorithm
@@ -164,10 +289,16 @@ var IRTAlgorithm = function(){
 
 // ======================TEST CASES========================= //
 
-result = findItemInBank(23);
-console.log("Item difficulty closest to 23");
-console.log(result.difficulty);
-console.log(result.question);
+begin();
+
+
+
+
+// findITEMINBANK Testing
+// result = findItemInBank(23);
+// console.log("Item difficulty closest to 23");
+// console.log(result.difficulty);
+// console.log(result.question);
 
 
 
