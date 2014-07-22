8861fabcd77a48e4b7f9165680591c8afd88a12e
Browenchen <owenchen@berkeley.edu>
2014-07-22 12:03:47 +0100

    implemented brinbaum models and three parameter IRT

--- itemResponse.js
+++ itemResponse.js
@@ -6,6 +6,9 @@ itemResponse.js
 Intelligently provides a pre-assessment algorithm to determine where a student 
 Should begin. 
 
+These models return P(theta), the probability that a user gets this item correct, given the laten ability
+of that user and the parameters for the item.
+
 */
 
 /*
@@ -20,6 +23,9 @@ answer to this specific problem given an ability level.
 Rasch Model IMplementation
 Equation => exp(theta - Bi) / 1 + exp(theta - Bi)
 
+References:
+Book Rasch Models
+
 ===========================TEST EXAMPLES===========================
 
 itemDif = 
@@ -27,16 +33,71 @@ itemDif =
 */
 	
 var raschModel = function(itemDifficulty, latentAbility){
-
 	result = ( Math.exp(latentAbility - itemDifficulty) / (1 + Math.exp(latentAbility - itemDifficulty)) ) 
 	return result;
 }
 
+/*
+@brinbaumModel
 
-console.log(raschModel(2, 3));
+Two parameter IRT with discrimination variable ai. 
+
+
+*/
+
+var brinbaumModel = function(itemDifficulty, latentAbility, discrimination){
+
+	var numerator = (Math.exp(discrimination * (latentAbility - itemDifficulty)));
+	var denominator = 1 + (Math.exp(discrimination * (latentAbility - itemDifficulty)));
+
+	result = (numerator / denominator);
+	return result;
+
+}
+
+
+
+/*
+@threeParamModel
+
+Three parameter IRT with third variable ci for pseudoChance. Lower bound to account for 
+guessing.
+
+*/
+
+var threeParamModel = function(itemDifficulty, latentAbility, discrimination, psuedoChance){
+
+	numerator = (1 - psuedoChance);
+	denominator = 1 + (Math.exp(-1.7 * (discrimination) * (latentAbility - itemDifficulty)));
+
+	result = psuedoChance + (numerator / denominator);
+
+	return result;
+
+}
+
+
+
+
+
+
 /*
 @maximumLikelihood
 
 Rasch model parameter updates
+Page 55 of the book.
+Maximum Likelihood Estimator, MLE of theta. 
 
 */
+
+
+
+
+// ========================TESTING ================================
+
+console.log("1 Parameter IRT");
+console.log(raschModel(3, 2));
+
+console.log("2 parameter IRT with no discrimination");
+console.log(brinbaumModel(3,2, 1));
+
